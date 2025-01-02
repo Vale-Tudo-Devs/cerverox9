d6885744745d44a2203f2518d26bf286a9ea2ebe
Vinicius Caldo <vcaldonoreply@github.com>
2025-01-02 19:35:31 +0100

    Fix UserStatsHandler to require at least one word for username validation

--- telegram/pkg/handlers/handlers.go
+++ telegram/pkg/handlers/handlers.go
@@ -109,7 +109,7 @@ func UserStatsHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
 	words := strings.Fields(messageText)
 	var targetUser string
 	log.Printf("words: %v", words)
-	if len(words) < 2 {
+	if len(words) < 1 {
 		b.SendMessage(ctx, &bot.SendMessageParams{
 			ChatID: update.Message.Chat.ID,
 			Text:   "Please provide a valid username",
