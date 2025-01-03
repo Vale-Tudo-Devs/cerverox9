7d13b5331c62a3b08c6467efd2bc0053ae9ace82
Vinicius Caldo <vcaldonoreply@github.com>
2025-01-03 16:57:24 +0100

    Remove commented-out code for guild ID and voice rank logging

--- discord/cmd/main.go
+++ discord/cmd/main.go
@@ -79,18 +79,18 @@ func main() {
 		}
 	}()
 
-	guildId, ok := os.LookupEnv("DISCORD_GUILD_ID")
-	if !ok {
-		log.Fatal("DISCORD_GUILD_ID env var is required")
-	}
+	// guildId, ok := os.LookupEnv("DISCORD_GUILD_ID")
+	// if !ok {
+	// 	log.Fatal("DISCORD_GUILD_ID env var is required")
+	// }
 
-	guildName, duration, voiceRank, err := dm.GetVoiceRank(guildId)
-	if err != nil {
-		log.Println("error getting voice rank", err)
-		return
-	}
+	// guildName, duration, voiceRank, err := dm.GetVoiceRank(guildId)
+	// if err != nil {
+	// 	log.Println("error getting voice rank", err)
+	// 	return
+	// }
 
-	log.Printf("Guild: %s, Duration: %s, Voice Rank: %s\n", guildName, duration, voiceRank)
+	// log.Printf("Guild: %s, Duration: %s, Voice Rank: %s\n", guildName, duration, voiceRank)
 
 	select {}
 }
